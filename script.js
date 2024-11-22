f38e5ccde6bdb5cc1b0c93a9fcc5d638a4f2efd0
Camilhg <ch442@njit.edu>
2024-11-21 20:02:49 -0500

    java

---
+++ script.js
@@ -0,0 +1,23 @@
+let username = "enter username"
+
+let userage = age
+
+
+console.log(username)
+console.log(userage)
+
+
+
+let eligableuser = true
+
+console.log(eligableuser)
+
+
+let studentgrade;
+console.log (studentgrade)
+
+
+let userdiscount; true
+console.log(userdiscount)
+
+let isMember = true;
